77eeeeb1750408164d909fc05b89cd86525f4e3c
Hack Reactor Students <github@hackreactor.com>
2013-10-04 15:47:19 -0700

    Adds user and post models to mongoDB

--- server/API.js
+++ server/API.js
@@ -1,6 +1,7 @@
 // API
 // ===
 
+
 module.exports.api = function(server, schema) {
 
   // Sample Rest Call
@@ -11,6 +12,16 @@ module.exports.api = function(server, schema) {
 
   server.get('/login', function(req, res) {
     res.send('<h1>login World!</h1>');
+  });
+
+  server.get('/userdata', function(req, res) {
+    //TO-DO: solidify schema
+    //schema.getUserPosts
+
+    //figure out how to get the userid of the client
+    schema.userGet(req, res, 'dummyUser');
+
+    //res.send(result);
   });
 
 };
