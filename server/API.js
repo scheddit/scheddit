8a82b052e57acb923340302f79e76052ff0b0283
Hack Reactor Students <github@hackreactor.com>
2013-10-03 17:03:58 -0700

    Add test and route for GET login endpoint.

--- server/API.js
+++ server/API.js
@@ -3,10 +3,14 @@
 
 module.exports.api = function(server, schema) {
 
-	// Sample Rest Call
+  // Sample Rest Call
 
-	server.get('/hello', function(req, res){
-		res.send('<h1>Hello World!</h1>');
-	});
+  server.get('/hello', function(req, res) {
+    res.send('<h1>Hello World!</h1>');
+  });
 
-}
+  server.get('/login', function(req, res) {
+    res.send('<h1>login World!</h1>');
+  });
+
+};
--- server/tests/specs/spec.js
+++ server/tests/specs/spec.js
@@ -1,26 +1,55 @@
 // SERVER SIDE TEST SUITE
 var request = require('superagent'),
-	expect = require('expect.js'),
-	app = require('../../server.js');
+  expect = require('expect.js'),
+  app = require('../../server.js');
 
 
 // BEFORE HOOK
 before(function(){
-	this.server = http.createServer(app).listen(3000);
+  this.server = http.createServer(app).listen(3000);
 });
 
 // AFTER HOOK
 after(function(done){
-	this.server.close(done);
+  this.server.close(done);
 });
 
 // TEST SUITES
 describe('API: GET /hello', function() {
-	it('Should Return 200', function(done) {
-		request.get('localhost:3000/hello').end(function(res) {
-			expect(res).to.exist;
-			expect(res.status).to.equal(200);
-			done();
-		});
-	});
+  it('Should Return 200', function(done) {
+    request.get('localhost:3000/hello').end(function(res) {
+      expect(res).to.exist;
+      expect(res.status).to.equal(200);
+      done();
+    });
+  });
 });
+
+//Write a test that will expect a 200 reply for enpoint /login GET
+describe('API: GET /login', function() {
+  it('Should Return 200', function(done) {
+    request.get('localhost:3000/login').end(function(res) {
+      expect(res).to.exist;
+      expect(res.status).to.equal(200);
+      done();
+    });
+  });
+});
+
+
+//Test for /login POST request response ( credentials exist in database )
+//add those test credentials either here or just manually to db
+
+//Test for unsuccessful response ( credentials that dont exist ) 401
+
+//Also neet to test oauth
+
+
+/*Test database*/
+//Test connection to db
+//Test succesful insert user
+//Test retreieve existent user (succesful)
+//Test retreieve non existent user (failure)
+
+//Test insert scheddit post data
+//Test retrieval scheddit post data
