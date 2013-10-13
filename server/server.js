d5971704019b4059598172915dcc348cd7064f78
Hack Reactor Students <github@hackreactor.com>
2013-10-13 15:37:46 -0700

    Correctly assigns Scope to Passport-Reddit Strategy.

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ var schema = require('./schemas/schema');
 //   serialize users into and deserialize users out of the session.  Typically,
 //   this will be as simple as storing the user ID when serializing, and finding
 //   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete Reddit profile is
+//   have a database of user records, the complete Reddit profile  is
 //   serialized and deserialized.
 passport.serializeUser(function(user, done) {
   //so this is where we get the user's info which we will store in the db
@@ -62,10 +62,12 @@ passport.deserializeUser(function(obj, done) {
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Reddit
 //   profile), and invoke a callback with a user object.
+
 var rStrategy = new redditStrategy({
     clientID: redditConsumerKey,
     clientSecret: redditConsumerSecret,
-    callbackURL: "http://localhost:1337/redirect"
+    callbackURL: "http://localhost:1337/redirect",
+    scope: ['submit', 'identity']
   },
   function(accessToken, refreshToken, profile, done) {
 
