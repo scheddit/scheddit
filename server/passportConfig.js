7c4b3b2dbb6811d39444bca3ad19367c0a6cc422
Hack Reactor Students <github@hackreactor.com>
2013-10-13 16:02:22 -0700

    Refactored Passport Config into passportConfig.js

---
+++ server/passportConfig.js
@@ -0,0 +1,71 @@
+var passport  = require('passport');
+var util      = require('util');
+var crypto    = require('crypto');
+var redditStrategy  = require('passport-reddit').Strategy;
+
+// DATABASE SCHEMAS
+// ================
+
+var schema = require('./schemas/schema');
+
+
+// ENV VARIABLE CONFIGURATION
+// ==========================
+
+var redditConsumerKey = process.env.REDDIT_API_KEY;
+var redditConsumerSecret = process.env.REDDIT_SECRET;
+
+// PASSPORT CONFIGURATION
+// ======================
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Reddit profile  is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  //so this is where we get the user's info which we will store in the db
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the RedditStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Reddit
+//   profile), and invoke a callback with a user object.
+
+var rStrategy = new redditStrategy({
+    clientID: redditConsumerKey,
+    clientSecret: redditConsumerSecret,
+    callbackURL: "http://localhost:1337/redirect",
+    scope: ['submit', 'identity']
+  },
+  function(accessToken, refreshToken, profile, done) {
+
+    var conditions = { "profile.id" : profile.id };
+    var update = {
+      profile: profile._json,
+      oauthInfo: {
+        accessToken: accessToken,
+        refreshToken: refreshToken
+      }
+    };
+
+    var options = { upsert: true};
+    //Update user document if found in databse,
+    //If not found, create document
+    // console.log("AccessToken:" + accessToken);
+    // console.log("Profile: " + profile.name);
+    schema.userModel.findOneAndUpdate(conditions, update, options, function(){
+      // console.log('user in database');
+    });
+    return done(null, profile);
+});
+
+
+module.exports = rStrategy;
--- server/server.js
+++ server/server.js
@@ -10,21 +10,16 @@ var app = module.exports = express();
 var mongoose  = require('mongoose');
 var API       = require('./API');
 var passport  = require('passport');
-var util      = require('util');
-var crypto    = require('crypto');
-var redditStrategy  = require('passport-reddit').Strategy;
-var request = require('request');
+var rStrategy = require('./passportConfig');
 
-// ENV VARIABLE CONFIGURATION
-// ==========================
+// DATABASE SCHEMAS
+// ================
 
-var redditConsumerKey = process.env.REDDIT_API_KEY;
-var redditConsumerSecret = process.env.REDDIT_SECRET;
+var schema = require('./schemas/schema');
 
 // DATABASE CONFIGURATION
 // ======================
 
-// Connect to Database
 mongoose.connect('mongodb://' + Config.database.IP + ':' +Config.database.port + '/' + Config.database.name);
 
 var db = mongoose.connection;
@@ -33,70 +28,8 @@ db.once('open', function callback () {
   console.log('Connected to ' + Config.database.name);
 });
 
-// DATABASE SCHEMAS
-// ================
-
-var schema = require('./schemas/schema');
-
-
-// PASSPORT CONFIGURATION
-// ======================
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete Reddit profile  is
-//   serialized and deserialized.
-passport.serializeUser(function(user, done) {
-  //so this is where we get the user's info which we will store in the db
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-
-// Use the RedditStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Reddit
-//   profile), and invoke a callback with a user object.
-
-var rStrategy = new redditStrategy({
-    clientID: redditConsumerKey,
-    clientSecret: redditConsumerSecret,
-    callbackURL: "http://localhost:1337/redirect",
-    scope: ['submit', 'identity']
-  },
-  function(accessToken, refreshToken, profile, done) {
-
-    var conditions = { "profile.id" : profile.id };
-    var update = {
-      profile: profile._json,
-      oauthInfo: {
-        accessToken: accessToken,
-        refreshToken: refreshToken
-      }
-    };
-
-    var options = { upsert: true};
-    //Update user document if found in databse,
-    //If not found, create document
-    // console.log("AccessToken:" + accessToken);
-    // console.log("Profile: " + profile.name);
-    schema.userModel.findOneAndUpdate(conditions, update, options, function(){
-      // console.log('user in database');
-    });
-    return done(null, profile);
-});
-
-passport.use(rStrategy);
-module.exports.rStrategy = rStrategy;
-
 // APP CONFIGURATION
 // ====================
-
 
 app.configure(function() {
   app.use(express["static"](__dirname + "/../public"));
@@ -119,6 +52,7 @@ app.configure(function() {
 // ===
 
 API.api(app, schema);
+passport.use('reddit', rStrategy);
 
 
 if (require.main === module) {
